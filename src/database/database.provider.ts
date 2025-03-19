5fff645f1164b43414b5046946196db82af5efe3
Raul Santamaria <raulsantamariao@hotmail.com>
2025-03-19 16:17:51 -0500

    Refactor configuration management: move envs to src/config, implement validation, and add global exception filter

--- src/database/database.provider.ts
+++ src/database/database.provider.ts
@@ -1,5 +1,5 @@
 import { Logger } from '@nestjs/common';
-import { envs } from 'config';
+import { envs } from 'src/config';
 import * as oracledb from 'oracledb';
 
 export const databaseProviders = [
